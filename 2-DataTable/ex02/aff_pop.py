9c5d575d47afe690edd4e585b4c09c031f75f3a8
Narek <narekkarapetyan015@gmail.com>
2025-11-13 14:29:11 +0400

    Dev | ex02 Done

--- 2-DataTable/ex02/aff_pop.py
+++ 2-DataTable/ex02/aff_pop.py
@@ -1,11 +1,27 @@
-from calendar import c
 import matplotlib.pyplot as plt
 from load_csv import Dataset
 
 
+def convert_population(value: str) -> list[float]:
+    """
+    Converts population strings
+    with suffixes 'k' and 'M' to float values.
+    """
+    result = []
+    for v in value:
+        if v.endswith('k'):
+            result.append(float(v[:-1]) / 1_000)
+        elif v.endswith('M'):
+            result.append(float(v[:-1]))
+        else:
+            result.append(float(v) / 1_000_000)
+    return result
+
+
 def aff_pop(data: Dataset, country: list[str]) -> None:
     """
-    Displays the population over the years for the specified country or countries.
+    Displays the population over the years
+    for the specified country or countries.
     """
     try:
         if data is None or data.data.empty:
@@ -13,7 +29,8 @@ def aff_pop(data: Dataset, country: list[str]) -> None:
         elif country is None or country == "" or len(country) == 0:
             raise ValueError("Country name must be provided.")
         elif not isinstance(country, list):
-            raise ValueError("Country parameter must be a list of country names.")
+            raise ValueError("Country parameter must",
+                             " be a list of country names.")
         elif len(country) != 2:
             raise ValueError("Please provide exactly two country names.")
         elif not all(isinstance(c, str) for c in country):
@@ -22,14 +39,20 @@ def aff_pop(data: Dataset, country: list[str]) -> None:
         if population_data.empty or population_data.shape[0] != 2:
             raise ValueError(f"No data found for the country: {country}")
         years = data.data.columns.astype(int)
-        life_expectancy = population_data.values
+        mask = years <= 2050
+        years = years[mask]
+        life_expectancy = population_data.values[:, mask]
+        life_expectancy = [convert_population(life_expectancy[i])
+                           for i in range(2)]
+        print(life_expectancy)
         plt.figure(figsize=(10, 5))
         plt.plot(years, life_expectancy[0], label=country[0], color='blue')
         plt.plot(years, life_expectancy[1], label=country[1], color='green')
         plt.title("Population Projections")
         plt.xlabel("Year")
-        plt.xticks(range(min(years), max(years) + 1 if max(years) < 2050 else 2050, 40))
+        plt.xticks(range(min(years), 2051, 40))
         plt.ylabel("Population")
+        plt.yticks([20, 40, 60], ['20M', '40M', '60M'])
         plt.legend()
         plt.grid(True)
         plt.savefig(f"{country[0]}_{country[1]}_population.png")
