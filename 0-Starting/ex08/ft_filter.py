bd70d48e792af7678f97a7775dadc2183f31e9b8
Narek <narekkarapetyan015@gmail.com>
2026-09-01 14:27:37 +0400

    Must have main

--- 0-Starting/ex08/ft_filter.py
+++ 0-Starting/ex08/ft_filter.py
@@ -9,3 +9,18 @@ is true. If function is None, return the items that are true."""
                 yield item
         elif function(item):
             yield item
+
+
+def main():
+    """Main function."""
+    try:
+        test_list = ["hello", "world", "this", "is", "a", "test"]
+        result = ft_filter(lambda x: len(x) > 3, test_list)
+        print(list(result))
+    except Exception:
+        print("AssertionError: the arguments are bad")
+
+
+if __name__ == "__main__":
+    """Main entry point of the script."""
+    main()
