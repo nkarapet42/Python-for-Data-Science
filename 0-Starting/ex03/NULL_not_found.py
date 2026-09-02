90fb9d689280282debbb85abcd1c44060abb3127
Narek <narekkarapetyan015@gmail.com>
2026-09-02 15:19:26 +0400

    Fixed

--- 0-Starting/ex03/NULL_not_found.py
+++ 0-Starting/ex03/NULL_not_found.py
@@ -5,12 +5,12 @@ def NULL_not_found(object: Any) -> int:
         print(f"Nothing: {object} {object.__class__}")
     elif object != object:
         print(f"Cheese: {object} {object.__class__}")
+    elif object is False:
+        print(f"Fake: {object} {object.__class__}")
     elif object == 0:
         print(f"Zero: {object} {object.__class__}")
     elif object == "":
         print(f"Empty: {object} {object.__class__}")
-    elif object is False:
-        print(f"Fake: {object} {object.__class__}")
     else:
         print("Type not Found")
         return 1
